18f23a5ff2e3066d9f131f0a8abb2235be4ff1eb
Coogies <coogies@gmail.com>
2025-05-23 14:33:05 +0900

    test for github ci/cd

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -3,13 +3,13 @@ import React from 'react';
 const About = () => (
   <div className="content-container">
     <div className="content-title-group not-found">
-      <h2 className="title">Product Wish List</h2>
+      <h2 className="title">Product Wish List SGKIM</h2>
       <p>
         This project was created to help represent a fundamental app written
-        with React. The shopping theme is used throughout the app.
+        with React. The shopping theme is used throughout the app. SGKIM
       </p>
       <br />
-      <h2 className="title">Resources</h2>
+      <h2 className="title">Resources SGKIM</h2>
       <ul>
         <li>
           <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
